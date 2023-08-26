f064d2b609b8a4570c7786ee4caad28579607d2b
gpranjan <gpranjan@gmail.com>
2023-08-26 18:18:11 +0530

    Code Snapshot

---
+++ src/simulation/branch_ops.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MAC BOOK on 26/08/23.
+//
+
+#include "branch_ops.h"
---
+++ src/simulation/branch_ops.h
@@ -0,0 +1,14 @@
+//
+// Created by MAC BOOK on 26/08/23.
+//
+
+#ifndef ATMEGASIM_SRC_BRANCH_OPS_H
+#define ATMEGASIM_SRC_BRANCH_OPS_H
+
+
+class BranchOps {
+
+};
+
+
+#endif //ATMEGASIM_SRC_BRANCH_OPS_H
