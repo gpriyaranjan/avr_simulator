6387886aac2347ae73784ec42c2b4d3fa04b803a
gpranjan <gpranjan@gmail.com>
2023-08-31 23:09:14 +0530

    Code Snapshot

--- src/simulation/ops/io_ops_impl.cpp
+++ src/simulation/ops/io_ops_impl.cpp
@@ -1,2 +1,26 @@
+#include "../infra/environment.h"
 #include "../gen2/io_ops_impl.h"
 
+void IoOpsImpl::IN(Environ &env, FiveBit tgtRegNum, SixBit portAddr) {
+    // Todo : Implement this in a meaningful way
+}
+
+void IoOpsImpl::OUT(Environ &env, FiveBit srcRegNum, SixBit portAddr) {
+    // Todo : Implement this in a meaningful way
+}
+
+void IoOpsImpl::SBI(Environ &env, FiveBit portAddr, ThreeBit bitNum) {
+    // Todo : Implement this in a meaningful way
+}
+
+void IoOpsImpl::CBI(Environ &env, FiveBit portAddr, ThreeBit bitNum) {
+    // Todo : Implement this in a meaningful way
+}
+
+void IoOpsImpl::SEI(Environ &env) {
+    env.sReg.setI(true);
+}
+
+void IoOpsImpl::CLI(Environ &env) {
+    env.sReg.setI(false);
+}
