01be721efbc00c476d593517ca6e5c0b62234f5a
gpranjan <gpranjan@gmail.com>
2023-09-02 17:33:27 +0530

    Code Snapshot

--- src/simulation/expt/alu_ops2.cpp
+++
@@ -1,9 +0,0 @@
-#include "../gen2/instrns.h"
-#include "alu_ops2.h"
-
-ShortInstrn AluOps2::ADD(FiveBit srcAddr, FiveBit tgtAddr) {
-    ShortInstrn code = InstrnEnum::ADD;
-    code = code | (srcAddr & 0x0F) | ( (srcAddr & 0x10 ) << 5 );
-    code = code | ( (tgtAddr & 0x0F) << 4 );
-    return code;
-}
